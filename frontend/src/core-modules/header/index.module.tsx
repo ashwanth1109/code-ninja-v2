4e881e0a7cc4caea31fcce610d3633e77e8dc7c2
Ashwanth A R <ashwanth1109@gmail.com>
2020-10-22 02:54:17 +0530

    feature: main container

--- frontend/src/core-modules/header/index.module.tsx
+++ frontend/src/core-modules/header/index.module.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Container from './container.styled';
 import Logo from './logo.component';
+import Main from './main.styled';
 
 const Header = () => {
   return (
     <Container>
       <Logo />
+      <Main>
+        <p>Main</p>
+      </Main>
     </Container>
   );
 };
---
+++ frontend/src/core-modules/header/main.styled.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Main = styled.div`
+  flex: 1;
+  justify-content: space-between;
+  margin: 0 16px;
+`;
+
+export default Main;
