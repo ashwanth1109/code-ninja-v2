571ba57a7eb7e9b151749f43bc5a79c2b80f3c7c
Ashwanth A R <ashwanth1109@gmail.com>
2020-11-08 01:55:05 +0530

    deploy: cdk code to upload build into s3 bucket and deploy cache invlidation to cloudfront

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { DeployStack } from '../lib/deploy-stack';
+import "source-map-support/register";
+import { App } from "@aws-cdk/core";
+import { DeployStack } from "../lib/deploy-stack";
 
-const app = new cdk.App();
-new DeployStack(app, 'DeployStack');
+const app = new App();
+new DeployStack(app, "code-ninja-deploy-stack");
--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,9 +1,34 @@
-import * as cdk from '@aws-cdk/core';
+import { Bucket, RedirectProtocol } from "@aws-cdk/aws-s3";
+import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
+import { Construct, RemovalPolicy, Stack, StackProps } from "@aws-cdk/core";
 
-export class DeployStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class DeployStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = "code-ninja.xyz";
+    const destinationBucket = new Bucket(this, bucketName, {
+      bucketName,
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+    });
+
+    const subDomainBucketName = "www.code-ninja.xyz";
+    new Bucket(this, subDomainBucketName, {
+      bucketName: subDomainBucketName,
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteRedirect: {
+        hostName: bucketName,
+        protocol: RedirectProtocol.HTTP,
+      },
+    });
+
+    new BucketDeployment(this, "DeployCodeNinja", {
+      destinationBucket,
+      sources: [Source.asset("../frontend/build")],
+    });
   }
 }
