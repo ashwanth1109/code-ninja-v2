f6c9df5399ff2490eb8c02903032ff0ecb11b8b6
Ashwanth A R <ashwanth1109@gmail.com>
2020-11-08 16:50:43 +0530

    feature: page responsiveness

--- frontend/src/core-modules/article/article.component.tsx
+++ frontend/src/core-modules/article/article.component.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import articles from '../../auto-generated/article';
+import styled from '@emotion/styled';
+
+const ArticleContainer = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  min-width: 400px;
+  padding: 16px;
+`;
 
 const Article = () => {
   const { slug }: { slug: string } = useParams();
 
-  return articles[slug]({});
+  return <ArticleContainer>{articles[slug]({})}</ArticleContainer>;
 };
 
 export default Article;
